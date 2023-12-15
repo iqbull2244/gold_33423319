e997bf2d9110f02c6f3663ce3a4bde8c2ed02f23
rafiiqbal2407 <rafiiqbal2407@gmail.com>
2023-12-15 18:06:00 +0700

    ..

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='gold33423324',
+    name='gold33423319',
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
